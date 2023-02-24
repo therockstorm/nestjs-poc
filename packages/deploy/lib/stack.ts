aac0c773cde02dccb730bed70833b8157e79e816
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-02-24 09:56:25 -0700

    fix extends

--- packages/deploy/lib/stack.ts
+++ packages/deploy/lib/stack.ts
@@ -1,8 +1,19 @@
-import * as cdk from "aws-cdk-lib";
+// import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-export class NestJsPocStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class NestJsPocStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // new apprunner.Service(this, "Service", {
+    //   source: apprunner.Source.fromGitHub({
+    //     branch: "main",
+    //     configurationSource: apprunner.ConfigurationSourceType.REPOSITORY,
+    //     connection:
+    //       apprunner.GitHubConnection.fromConnectionArn("CONNECTION_ARN"),
+    //     repositoryUrl: "https://github.com/aws-containers/hello-app-runner",
+    //   }),
+    // });
   }
 }
