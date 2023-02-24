9f7c857af89b4e3bc2758d65bfbf7b4ba0b822de
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-02-24 10:16:43 -0700

    fix dist

--- packages/deploy/lib/stack.ts
+++ packages/deploy/lib/stack.ts
@@ -1,19 +1,21 @@
-// import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class NestJsPocStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // new apprunner.Service(this, "Service", {
-    //   source: apprunner.Source.fromGitHub({
-    //     branch: "main",
-    //     configurationSource: apprunner.ConfigurationSourceType.REPOSITORY,
-    //     connection:
-    //       apprunner.GitHubConnection.fromConnectionArn("CONNECTION_ARN"),
-    //     repositoryUrl: "https://github.com/aws-containers/hello-app-runner",
-    //   }),
-    // });
   }
+}
+
+export interface ResourceIdReq {
+  /**
+   * A friendly camelCase name to distinguish from others of the same resource
+   * type. Doesn't have to be unique unless AWS requires it.
+   */
+  readonly name: string;
+
+  /**
+   * The camelCase AWS resource you're creating, e.g., `stack`, `bucket`.
+   */
+  readonly resource: string;
 }
--- packages/service/src/app.module.js
+++ packages/service/src/app.module.js
@@ -5,21 +5,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
-var common_1 = require("@nestjs/common");
-var app_controller_1 = require("./app.controller");
-var app_service_1 = require("./app.service");
-var AppModule = /** @class */ (function () {
-    function AppModule() {
-    }
-    AppModule = __decorate([
-        (0, common_1.Module)({
-            controllers: [app_controller_1.AppController],
-            imports: [],
-            providers: [app_service_1.AppService]
-        })
-    ], AppModule);
-    return AppModule;
-}());
+const common_1 = require("@nestjs/common");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+let AppModule = class AppModule {
+};
+AppModule = __decorate([
+    (0, common_1.Module)({
+        controllers: [app_controller_1.AppController],
+        imports: [],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
 exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
